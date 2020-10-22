275857ca82a9360946572075182f43b198db6580
ali <alisamymohamed@yahoo.com>
2020-10-22 05:32:01 -0700

    setup navigation

---
+++ app/src/main/java/com/ali/madarsofttask/presentation/ShowUsersFragment.kt
@@ -0,0 +1,11 @@
+package com.ali.madarsofttask.presentation
+
+import androidx.fragment.app.Fragment
+import com.ali.madarsofttask.R
+
+
+class ShowUsersFragment : Fragment(R.layout.fragment_show_users) {
+
+
+
+}
